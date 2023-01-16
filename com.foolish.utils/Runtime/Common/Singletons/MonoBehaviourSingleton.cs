19e0ef211d9a918d76b03f12d13709efa3d376d3
Foolish-Mac <>
2023-01-16 17:30:01 +0700

    - add singletons

---
+++ com.foolish.utils/Runtime/Common/Singletons/MonoBehaviourSingleton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Utils.Common.Singletons
+{
+    /// <summary>
+    /// Generic temporary Singleton for MonoBehaviour
+    /// </summary>
+    public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance { get; protected set; }
+
+        [SerializeField]
+        private bool needUnparentSelf = true;
+
+        protected virtual void Awake()
+        {
+            if (Instance != null)
+                Destroy(Instance);
+            Instance = this as T;
+            if (needUnparentSelf)
+                transform.parent = null;
+            Init();
+        }
+
+        protected abstract void Init();
+    }
+}
---
+++ com.foolish.utils/Runtime/Common/Singletons/MonoBehaviourSingletonPersistentLazy.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace Utils.Common.Singletons
+{
+
+    /// <summary>
+    /// Generic Lazy Singleton for MonoBehaviour
+    /// </summary>
+    public abstract class MonoBehaviourSingletonPersistentLazy<T> : MonoBehaviour where T : Component
+    {
+        protected static T instance = null;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameObject(instance.GetType().Name).AddComponent<T>();
+                    DontDestroyOnLoad(instance);
+                    (instance as MonoBehaviourSingletonPersistentLazy<T>).Init();
+                }
+                return instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            if (instance == null)
+            {
+                transform.parent = null;
+                instance = this as T;
+                DontDestroyOnLoad(this);
+                Init();
+            }
+            else
+            {
+                if (instance != this)
+                    Destroy(gameObject);
+            }
+        }
+        protected abstract void Init();
+    }
+}
